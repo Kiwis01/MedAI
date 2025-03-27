eab1c1c9a53f9609111afd4ff0f72adea652ba40
Kiwis01 <chquihui@asu.edu>
2025-03-27 12:13:14 -0700

    gemini implementation

--- api.py
+++ api.py
@@ -1,5 +1,8 @@
 import requests
 import cv2
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 def pred(filepath):
     url = "https://medai-api-f34085d124bb.herokuapp.com/uploadfile/"
@@ -42,3 +45,65 @@ def draw_box(response, filepath):
 
     return image, file_name
 
+def chat(text, prediction=None):
+    GEMINI_KEY = os.getenv("GEMINI_KEY")
+    url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key={GEMINI_KEY}"
+    headers = {
+        "Content-Type": "application/json"
+    }
+    data = {
+        "generationConfig": {
+            "stopSequences": [
+                "Title"
+            ],
+            "temperature": 1.0,
+            "maxOutputTokens":200,
+            "topP": 0.8,
+            "topK": 10
+        },
+        "system_instruction": {
+        "parts": [
+        { "text": "You are collaborating with medical professionals to assist users in understanding their symptoms. Your primary goal is to provide honest, accurate, and reliable information based on the listed symptoms. Ensure that your responses are clear, medically sound, and aligned with best practices in healthcare. Make educated diagnoses or medical claims; but also encourage users to consult a qualified healthcare provider for professional evaluation and treatment when necessary."},
+        { "text": "Keep in mind that the output should not exceed 200 tokens. Ensure that responses are concise, relevant, and well-structured while maintaining clarity and accuracy."},
+        { "text": f"Consider that the tumor prediction from the image is {prediction}. Make sure that you are also considering confidence scores since low confidence scores should make the tumor even less likely to be present, however do not skip on informing the user if there is a posibility of a tumor being present. The prediction has 5 possible values: Pituitary, Glioma, Meningioma, No Tumor, and None (default). If the prediction is none it means that the user has not utilized the classification model yet, if that is the case and the symptoms listed by the user seem to be correlated to a brain tumor, suggest the user to upload a medical image so the web application can perform a classification task and possibly predict a tumor."},
+        # { "text": "Make sure to not mention exact results coming from the prediction value, use high, low, medium to represent the confidence score"}
+            ]
+        },
+
+        "contents": {
+        "parts": {
+            "text": text}
+        }
+    }
+
+    response = requests.post(url, json=data, headers=headers)
+    return(response)
+
+#TODO: We need at least 3 studies by medical professionals regarding information of each of the three tumors, pituitary, meningioma, glioma.
+    # chatbot
+        # Add chatbot into html design
+        # Implement conversation awareness
+    
+    # Vector Embedding 
+        # embed pdf medical files
+
+    # Add more instructions
+        # Instructions to evaluate medical files stored in vector embeddings
+
+    # Add login auth
+
+    # Add database
+        # store medical responses on database 
+
+
+if __name__ == "__main__":
+    response = chat("I am sick, Im having strong headaches, I had seizure in the last two weeks, and I have been nauseous all day. What could be the issue? ", prediction="None")
+    response_data = response.json()
+    response_text = response_data["candidates"][0]["content"]["parts"][0]["text"]
+    usage_metadata = response_data["usageMetadata"]
+
+    print("Response Text:")
+    print(response_text)
+
+    print("\nUsage Metadata:")
+    print(usage_metadata)
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, send_from_directory
 import os
 from werkzeug.utils import secure_filename
 import shutil
-from api import pred, draw_box
+from api import pred, draw_box, chat
 import cv2
 
 app = Flask(__name__)
@@ -28,7 +28,6 @@ def clear_folder():
     if os.path.exists(predict_path):
         shutil.rmtree(predict_path)
     os.makedirs(predict_path)
-
 
 @app.route('/')
 def home():
@@ -59,10 +58,8 @@ def upload_image():
         
     return jsonify({'error': 'Please try again, Allowed image types are - png and jpg'})
 
-
 if __name__ == "__main__":
     clear_folder()
-    app.run(host="0.0.0.0", port=8011)
-
+    app.run(host="0.0.0.0", port=8012)
 
 #TODO: Need to add a scheduler to clean /static/predict/ and /static/uploads/ folders every week or so. We can connect these folder data to a database. 
